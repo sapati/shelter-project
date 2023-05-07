81839be86075eed40e006ac9a11cf94d607521ed
Sandor Apati <s.apati@mito.hu>
2023-05-07 16:02:10 +0200

    add clap, dotenv and anyhow

--- shelter_main/src/main.rs
+++ shelter_main/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, Command};
+use dotenv::dotenv;
+
+pub fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let command = Command::new("Dog Shelter sample application")
+        .version("1.0")
+        .author("Sandor Apati <sapati@gmail.com>")
+        .about("A sample application to experiment with Rust-based microservices")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Configuration file location")
+                .default_value("config.json"),
+        );
+
+    let _matches = command.get_matches();
+
+    Ok(())
 }
